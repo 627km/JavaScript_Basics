8f3aba812003284997ad64c4b36e2e8a8e087731
627km <86594128+627km@users.noreply.github.com>
2021-08-09 16:55:25 +0900

    Update array'sMethod.js

--- 배열/array'sMethod/array'sMethod.js
+++ 배열/array'sMethod/array'sMethod.js
@@ -32,17 +32,36 @@ family.splice(family.length, 0, '액션가면');
 let country = ['한국', '중국','일본', '미국', '영국', '러시아', '호주']; 
 // 배열의 첫 요소를 삭제: shift()
 country.shift();  // 배열의 첫 요소를 삭제하고 나머지는 앞으로 밀린다. 
-console.log(country);  // [중국 일본 미국 영국 러시아 호주] 출력
+console.log(country);
 
 // 배열의 마지막 요를 삭제: pop()
 country.pop();
-console.log(country);  // [중국 일본 미국 영국 러시아] 출력
+console.log(country);
 
 //배열의 첫 요소로 값 추가: unshift()
 country.unshift('프랑스');
-console.log(country);  // [프랑스 중국 일본 미국 영국 러시아] 출력
+console.log(country);
 
 // 배열의 마지막 요소로 값 추가: push()
 country.push('체코');
-console.log(country);  // [프랑스 중국 일본 미국 영국 러시아 체코] 출력
+console.log(country);
 
+// 배열에서 특정 값 찾기 (indexOf / lastIndexOf)
+
+let brands = ['Google', 'Kakao', 'Naver', 'Kakao'];
+
+console.log(brands.indexOf('Kakao'));  // 배열에 Kakao가 있으므로 앞에서부터 처음에 발견 된 인덱스(1)가 리턴된다.  
+console.log(brands.indexOf('Daum'));  // 배열에 Daum이라는 값이 없기때문에 -1이 출력된다.
+
+// lastIndexOf 메소드는 indexOf 와 반대로 탐색을 뒤에서 부터 한다. 
+console.log(brands.lastIndexOf('Kakao')); // 배열에 Kakao가 있는데 뒤에서 부터 탐색하므로 뒤에서부터 처음 발견 된 인덱스(3)이 리턴된다.
+console.log(brands.lastIndexOf('Daum')); // 배열에 뒤에서 부터 탐색해도 Daum이라는 값은 없기때문에 -1이 출력된다.
+
+// 배열에서 특정 값이 있는지 확인하기 (includes)
+console.log(brands.includes('Kakao')); // 단순히 있는지 없는지 확인해준다. true
+console.log(brands.includes('Daum')); // false
+
+// 배열 뒤집기 (reverse)
+console.log(brands);  // ["Google", "Kakao", "Naver", "Kakao"]
+brands.reverse();  // 순서를 뒤집어준다.
+console.log(brands);  // ["Kakao", "Naver", "Kakao", "Google"]
