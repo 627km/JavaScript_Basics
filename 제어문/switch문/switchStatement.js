f1a8abd17a34d6672bf91986bba3f44e9fc462e5
627km <rudals_jeon@naver.com>
2022-03-01 17:41:50 +0900

    update

--- 제어문/switch문/switchStatement.js
+++ 제어문/switch문/switchStatement.js
@@ -7,7 +7,7 @@ switch (비교할 값) {
   case 조건값_2:
     동작부분;
     break;
-  default:  // 비교할 값이 모든 모든 조건값과 일치하지 않을 때 동작할 코드 
+  default:  // 비교할 값이 모든 모든 조건값과 일치하지 않을 때 동작할 코드 (default 키워드 부분은 생략가능)
     동작부분;
 }
 */
@@ -50,3 +50,30 @@ switch(myChoice) {
   default:
     console.log('1에서 4사이의 숫자를 선택해 주세요.');
 }
+
+// switch 조건문으로 홀수와 짝수를 구분
+var input = Number(prompt('숫자를 입력하세요.', '숫자'));  // 변수를 선언
+
+switch (input % 2) {
+  case 0:
+    alert('짝수입니다.');
+    break;
+  case 1:
+    alert('홀수입니다.');
+    break;
+  default:
+    alert('숫자가 아닙니다.');
+    break;
+}
+
+// 삼항 연산자 (삼항 연산자는 코드를 짧게 쓰려는 개발자가 자주 사용)
+/* 
+<불 표현식> ? <참일 때 실행하는 문장> : <거짓일 때 실행하는 문장>
+*/
+
+// 변수선언
+var input = prompt('숫자를 입력하세요', '');
+var number = Number(input);
+
+// 조건문
+(number > 0) ? alert('자연수입니다.') : alert('자연수가 아닙니다.');
